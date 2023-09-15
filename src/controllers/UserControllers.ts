1ce6a89cf91e7cf59f65455ae84476d06ef692d8
Bhushan Alhat <alhatbhushan@gmail.com>
2023-09-15 16:58:56 +0530

    basic CRUD functionality

--- src/controllers/UserControllers.ts
+++ src/controllers/UserControllers.ts
@@ -1,36 +1,68 @@
 import { Request, Response } from 'express';
+import { prisma } from '../db/prisma';
 
 // @desc Get all users
 // @route GET /api/users
 // @access public
-export const getAllUsers = (req: Request, res: Response) => {
-	res.send('get all users');
+export const getAllUsers = async (req: Request, res: Response) => {
+	const users = await prisma.user.findMany();
+
+	res.status(200).json({ success: true, count: users.length, data: users });
 };
 
 // @desc Create user
 // @route POST /api/users
 // @access public
-export const createUser = (req: Request, res: Response) => {
-	res.send('create user');
+export const createUser = async (req: Request, res: Response) => {
+	const { username, email, password, displayName } = req.body;
+
+	// todo perform validation
+	// todo hash password before saving to database
+	// todo check for unique emails
+	const user = await prisma.user.create({
+		data: { username, email, displayName, hashedPassword: password },
+	});
+
+	res.status(200).json({ success: true, data: user });
 };
 
 // @desc Get single user
 // @route GET /api/users/:id
 // @access public
-export const getUser = (req: Request, res: Response) => {
-	res.send('get user');
+export const getUser = async (req: Request, res: Response) => {
+	const { id } = req.params;
+
+	const user = await prisma.user.findUnique({ where: { userId: id } });
+
+	res.status(200).json({ success: true, data: user });
 };
+
+// todo add route to change password
 
 // @desc Update single user
 // @route PUT /api/users/:id
 // @access private
-export const updateUser = (req: Request, res: Response) => {
-	res.send('update user');
+export const updateUser = async (req: Request, res: Response) => {
+	const { id } = req.params;
+	const { username, email, displayName } = req.body;
+
+	// todo perform validation
+	// todo check for unique emails
+	const user = await prisma.user.update({
+		where: { userId: id },
+		data: { username, email, displayName },
+	});
+
+	res.status(200).json({ success: true, data: user });
 };
 
 // @desc Delete single user
 // @route DELETE /api/users/:id
 // @access private
-export const deleteUser = (req: Request, res: Response) => {
-	res.send('delete user');
+export const deleteUser = async (req: Request, res: Response) => {
+	const { id } = req.params;
+
+	const user = await prisma.user.delete({ where: { userId: id } });
+
+	res.status(200).json({ success: true, data: user });
 };

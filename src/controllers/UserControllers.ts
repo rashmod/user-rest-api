1b1ab771b410552e652ec62a6201f6b157877b1d
Bhushan Alhat <alhatbhushan@gmail.com>
2023-09-16 11:26:26 +0530

    added route and controller for change password

--- src/controllers/UserControllers.ts
+++ src/controllers/UserControllers.ts
@@ -3,11 +3,12 @@ import { validationResult } from 'express-validator';
 
 import { prisma } from '../db/prisma';
 import {
+	CustomError,
 	DuplicateEmailError,
 	NotFoundError,
 	ValidationError,
 } from '../utilities/Errors';
-import { hashPassword } from '../utilities/passwordUtils';
+import { comparePassword, hashPassword } from '../utilities/passwordUtils';
 
 // @desc Get all users
 // @route GET /api/users
@@ -16,6 +17,21 @@ export const getAllUsers = async (req: Request, res: Response) => {
 	const users = await prisma.user.findMany();
 
 	res.status(200).json({ success: true, count: users.length, data: users });
+};
+
+// @desc Get single user
+// @route GET /api/users/:id
+// @access public
+export const getUser = async (req: Request, res: Response) => {
+	const { id } = req.params;
+
+	const user = await prisma.user.findUnique({ where: { userId: id } });
+
+	if (!user) {
+		throw new NotFoundError('The requested user was not found');
+	}
+
+	res.status(200).json({ success: true, data: user });
 };
 
 // @desc Create user
@@ -40,21 +56,6 @@ export const createUser = async (req: Request, res: Response) => {
 	const user = await prisma.user.create({
 		data: { username, email, displayName, hashedPassword },
 	});
-
-	res.status(200).json({ success: true, data: user });
-};
-
-// @desc Get single user
-// @route GET /api/users/:id
-// @access public
-export const getUser = async (req: Request, res: Response) => {
-	const { id } = req.params;
-
-	const user = await prisma.user.findUnique({ where: { userId: id } });
-
-	if (!user) {
-		throw new NotFoundError('The requested user was not found');
-	}
 
 	res.status(200).json({ success: true, data: user });
 };
@@ -109,4 +110,55 @@ export const deleteUser = async (req: Request, res: Response) => {
 	const deletedUser = await prisma.user.delete({ where: { userId: id } });
 
 	res.status(200).json({ success: true, data: deletedUser });
+};
+
+// @desc Update password
+// @route PATCH /api/users/:id/changePassword
+// @access private
+export const updatePassword = async (req: Request, res: Response) => {
+	const { id } = req.params;
+	const { oldPassword, newPassword } = req.body;
+
+	const result = validationResult(req);
+	if (!result.isEmpty()) {
+		throw new ValidationError('Validation failed', result.array());
+	}
+
+	const user = await prisma.user.findUnique({ where: { userId: id } });
+
+	if (!user) {
+		throw new NotFoundError('The requested user was not found');
+	}
+
+	const isOldPasswordMatch = await comparePassword(
+		oldPassword,
+		user.hashedPassword
+	);
+
+	if (!isOldPasswordMatch) {
+		throw new CustomError(
+			'The old password provided does not match the current password associated with this user.',
+			401,
+			'InvalidCredentials'
+		);
+	}
+
+	const isNewPasswordSame = oldPassword === newPassword;
+
+	if (isNewPasswordSame) {
+		throw new CustomError(
+			'The new password cannot be the same as the old password. Please choose a different password.',
+			400,
+			'PasswordUnchanged'
+		);
+	}
+
+	const hashedPassword = await hashPassword(newPassword);
+
+	const updatedUser = await prisma.user.update({
+		where: { userId: id },
+		data: { hashedPassword },
+	});
+
+	res.status(200).json({ success: true, data: updatedUser });
 };
--- src/routes/UserRoutes.ts
+++ src/routes/UserRoutes.ts
@@ -4,11 +4,15 @@ import {
 	deleteUser,
 	getAllUsers,
 	getUser,
+	updatePassword,
 	updateUser,
 } from '../controllers/UserControllers';
 import catchAsyncError from '../utilities/catchAsyncError';
 import userValidation from '../validation/userValidation';
-import { passwordValidation } from '../validation/passwordValidation';
+import {
+	changePasswordValidation,
+	passwordValidation,
+} from '../validation/passwordValidation';
 
 const router = express.Router();
 
@@ -23,5 +27,11 @@ router.post(
 router.get('/:id', catchAsyncError(getUser));
 router.put('/:id', userValidation, catchAsyncError(updateUser));
 router.delete('/:id', catchAsyncError(deleteUser));
+
+router.patch(
+	'/:id/changePassword',
+	changePasswordValidation,
+	catchAsyncError(updatePassword)
+);
 
 export default router;
